f0d40996b96447f00299a4d46871ecb5bf7447d3
rohan deo <rdeo96@gmail.com>
2017-11-02 21:48:01 +0100

    open shift url removed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 app.set('view engine', 'ejs');
 
 const PORT=process.env.OPENSHIFT_NODEJS_PORT || 3000;
-const IPADDRESS=process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+const IPADDRESS= '127.0.0.1';
 
 var urlencodedParser = parser.urlencoded({ extended: false })
 
